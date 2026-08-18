0f6ed87fc63abf822bc3889804dc9f81865f06fd
aGROWLz <2669090429@qq.com>
2026-08-18 17:20:42 +0800

    feat: 初始化前端项目 - Vite + React + TypeScript

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
